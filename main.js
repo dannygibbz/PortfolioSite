31acbbcfd7cb225d3000011dfe0680839bd865b5
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-12-29 18:41:19 -0500

    added a success message for contact submission

--- main.js
+++ main.js
@@ -32,3 +32,44 @@ $(document).ready(function() {
     duration: 1800,
   })
 })
+
+window.addEventListener("DOMContentLoaded", function() {
+  var form = document.getElementById("my-form")
+  var status = document.getElementById("my-form-status")
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset()
+    status.innerHTML = "Thanks! Your message was submitted successfully."
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem."
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault()
+    var data = new FormData(form)
+    ajax(form.method, form.action, data, success, error)
+  })
+})
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest()
+  xhr.open(method, url)
+  xhr.setRequestHeader("Accept", "application/json")
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType)
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType)
+    }
+  }
+  xhr.send(data)
+}
